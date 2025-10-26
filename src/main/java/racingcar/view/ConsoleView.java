9c1c1da95c792624f5cdd1420b99b62401ad66f3
donggyun-han <hdg5639@daum.net>
2025-10-26 21:38:18 +0900

    refactor: ConsoleView 메소드 구현 - 사용 메소드 분리   - Name, Count 입력   - Round 결과 출력

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -1,10 +1,34 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ConsoleView {
-    public String input() {
-        return Console.readLine();
+    public List<String> inputNames() {
+        output("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Arrays.stream(Console.readLine().split(",")).toList();
+    }
+
+    public int inputCount() {
+        output("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public void printRound(List<Car> carList) {
+        for (Car car : carList) {
+            carDistance(car);
+        }
+    }
+
+    private void carDistance(Car car) {
+        String line = car.getName() +
+                " : " +
+                "-".repeat(Math.max(0, car.getDistance()));
+
+        output(line);
     }
 
     public void output(String output) {
