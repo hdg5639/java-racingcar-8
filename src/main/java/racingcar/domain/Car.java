43646e0131055575ea223999ac83eaf0edf6262c
donggyun-han <hdg5639@daum.net>
2025-10-26 20:55:52 +0900

    refactor: 불필요한 도메인 분리 제거 - Player, Car 도메인 병합

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,7 +3,27 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private int distance = 0;
+    private final String name;
+    private int distance;
+    private int winCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+        this.winCount = 0;
+    }
+
+    public void addWin() {
+        winCount++;
+    }
+
+    public int getWinCount() {
+        return winCount;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     // 거리 설정
     public void setDistance() {
@@ -25,5 +45,10 @@ public class Car {
     // 거리 조회
     public int getDistance() {
         return distance;
+    }
+
+    // 거리 초기화
+    public void initDistance() {
+        this.distance = 0;
     }
 }
